80c33f93be603507a323df7f78ef72fd04fa3804
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-10 13:46:57 +0200

    completed homework02

--- homework02/js/main.js
+++ homework02/js/main.js
@@ -1,5 +1,12 @@
-const age = prompt('How old are you');
-console.log (age);
+let n = parseInt(prompt("Enter number N:"));
+let m = parseInt(prompt("Enter number M:"));
+let skipEvenNumber = confirm("Skip even numbers?");
 
-const isAccessAllowed = age > 18 ? 'yes' : 'no'
-console.log(isAccessAllowed);
+let sum = 0;
+for (let i = n; i <= m; i++) {
+  if (!skipEvenNumber || i % 2 !== 0) {
+    sum += i;
+  }
+}
+
+document.writeln(`The sum of number ${n} and ${m} is ${sum}`);
