7c706d98b6eda47bae495182a7bd31d0e39339fa
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-14 15:16:10 +0200

    function 01 done

--- homework03/js/main.js
+++ homework03/js/main.js
@@ -1,4 +1,4 @@
-/*Створити функцію getMaxDigit(number) – яка отримує будь-яке
+/*Створити функцію getMaxDigit(number), яка отримує будь-яке
 число та виводить найбільшу цифру в цьому числі.*/
 
 function getMaxDigit(number) {
