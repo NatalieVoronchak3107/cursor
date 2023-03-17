d09a99dbc87fdb4cad7f9b6fdfcba01937e133df
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-17 15:17:31 +0200

    revised task 02

--- homework04/js/main.js
+++ homework04/js/main.js
@@ -1,7 +1,9 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій",
-"Світлана"];
-const topics = ["Диференційне рівняння", "Теорія автоматів",
-"Алгоритми і структури даних"];
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const topics = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
 const marks = [4, 5, 5, 3, 4, 5];
 
 /* 01. Розділіть студентів на пари(хлопець + дівчина) для работи над
@@ -9,45 +11,52 @@ const marks = [4, 5, 5, 3, 4, 5];
 [["Олександр", "Олена"], [..], [...]]; */
 
 const divideIntoPairs = (students) => {
-    const maleStudents = [];
-    const femaleStudents = [];
-    
-    for (let i = 0; i < students.length; i++) {
-      if (students[i].endsWith('а')) {
-        femaleStudents.push(students[i]);
-      } else {
-        maleStudents.push(students[i]);
-      }
+  const maleStudents = [];
+  const femaleStudents = [];
+
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].endsWith("а")) {
+      femaleStudents.push(students[i]);
+    } else {
+      maleStudents.push(students[i]);
     }
-    
-    const pairs = [];
-    for (let i = 0; i < maleStudents.length; i++) {
-      pairs.push([maleStudents[i], femaleStudents[i]]);
-    }
-    
-    return pairs;
   }
-  
-  const studentPairs = divideIntoPairs(students);
-  console.log(studentPairs);
+
+  const pairs = [];
+  for (let i = 0; i < maleStudents.length; i++) {
+    pairs.push([maleStudents[i], femaleStudents[i]]);
+  }
+
+  return pairs;
+};
+
+const studentPairs = divideIntoPairs(students);
+console.log(studentPairs);
 
 /* 02. Зіставте пари з попереднього завдання та теми проєктів, над
 якими студенти будуть працювати.
 Повинен вийти вкладений масив виду: [["Олександр і Олена",
 "Теорія автоматів"], [...], [...]] */
 
+const assignTopics = (studentPairs, topics) => {
+  const assignedTopicsToPairs = [];
 
+  for (let i = 0; i < studentPairs.length; i++) {
+    assignedTopicsToPairs.push([studentPairs[i].join(" і "), topics[i]]);
+  }
+
+  return assignedTopicsToPairs;
+};
+
+const assignedTopicsToPairs = assignTopics(studentPairs, topics);
+console.log(assignedTopicsToPairs);
 
 /* 03. Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...],
 [...]] */
 
-
-
 /* 04. Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт
 (тут функція буде не чистою, але не повинна мутувати массив):
 [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]] */
-
-
 
 /* const pairsTheme = getThemes(pairs, themes);
 const studentMarks = getMarks(students, marks);
