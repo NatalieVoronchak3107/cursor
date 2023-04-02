d17e7f94dc242b82d7bb4e2b63de4724e9cdce47
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-02 12:06:29 +0300

    advanced task done

--- homework08/js/main.js
+++ homework08/js/main.js
@@ -11,60 +11,66 @@ class Student {
   }
 
   getInfo() {
-    console.log(`Студент ${this.course}го курсу ${this.university}, ${this.fullName}`);
+    console.log(
+      `Студент ${this.course}го курсу ${this.university}, ${this.fullName}`
+    );
   }
 
-/* 2. Створіть геттер оцінок this.marks, який повертає масив оцінок студента [5, 4, 4, 5] */
+  /* 2. Створіть геттер оцінок this.marks, який повертає масив оцінок студента [5, 4, 4, 5] */
 
   get marks() {
     if (this.dismissed) {
-        return null;
-      }
+      return null;
+    }
     return this._marks;
   }
 
-/* 3. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту. Після того, як оцінка поставлена, геттер повинен повернути масив
+  /* 3. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту. Після того, як оцінка поставлена, геттер повинен повернути масив
 this.marks -> [5, 4, 4, 5, 5] */
 
   set marks(marks) {
     if (this.dismissed) {
-        return null;
-      }
+      return null;
+    }
     this._marks.push(marks);
   }
 
-/* 4. Створіть метод отримання середнього балу this.getAverageMark() -> 4.6 */
+  /* 4. Створіть метод отримання середнього балу this.getAverageMark() -> 4.6 */
 
-getAverageMark() {
+  getAverageMark() {
     if (this.dismissed) {
-        return null;
-      }
+      return null;
+    }
     let sum = 0;
     for (let i = 0; i < this._marks.length; i++) {
-    sum += this._marks[i];
+      sum += this._marks[i];
     }
     return (sum / this._marks.length).toFixed(1);
-    }
+  }
 
-/* 5. Створіть метод this.dismiss, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких
+  /* 5. Створіть метод this.dismiss, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких
 помилок, просто повертається завжди null замість масиву оцінок) */
 
-dismiss() {
+  dismiss() {
     this.dismissed = true;
     this._marks = null;
   }
 
-/* 6. Створіть метод this.recover, який дозволить поновити студента */
+  /* 6. Створіть метод this.recover, який дозволить поновити студента */
 
-recover() {
+  recover() {
     this.dismissed = false;
   }
 }
-let student = new Student("Вищої Школи Психотерапії м.Одеса",1,"Остап Родоманський Бендер");
+let student = new Student(
+  "Вищої Школи Психотерапії м.Одеса",
+  1,
+  "Остап Родоманський Бендер"
+);
 student.getInfo();
 console.log(student.marks);
 student.marks = 5;
-console.log(student.marks)
+console.log(student.marks);
 console.log(student.getAverageMark());
 student.dismiss();
 console.log(student.marks);
@@ -75,13 +81,37 @@ console.log(`Студент ${student.course}го курсу ${student.universit
 
 /* 1. Створіть новий клас BudgetStudent, який повністю наслідує клас Student */
 
-
+class BudgetStudent extends Student {
+    constructor(university, course, fullName) {
+      super(university, course, fullName);
+      this._scholarship = 1400;
+      this.scholarshipInterval = setInterval(() => this.getScholarship(), 30000);
+}
 
 /* 2. Бюджетний студент може отримати стипендію за допомогою методу this.getScholarship.
 Отримання стипендії супроводжується виведенням інформації в консоль: Ви отримали 1400 грн. стипендії */
 
+getScholarship() {
+    if (this.dismissed || this.getAverageMark() < 4) {
+      console.log('Ви не отримали стипендію');
+    } else {
+      console.log(`Ви отримали ${this._scholarship} грн. стипендії`);
+    }
+  }
+
+dismiss() {
+    super.dismiss();
+    clearInterval(this.scholarshipInterval);
+}
+}
+
 /* 3. Метод отримання стипендії автоматично викликається кожні 30 секунд післе створення об'єкту. Підказка: викликайте його в constructor.*/
-
 /* 4. Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0 */
-
 /* 5. Якщо студента виключено, він не отримує стипендію (думаю це було і так очевидно :) */
+    
+let budgetStudent = new BudgetStudent("Київський Політехнічний Інститут", 2, "Олександр Войцехівський");
+budgetStudent.getInfo();
+budgetStudent.getScholarship();
+
+budgetStudent.dismiss();
+budgetStudent.getScholarship();
