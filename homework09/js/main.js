f623c5a0f83e388331d4e72cbd723480a10aa3b4
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-03 23:01:24 +0300

    hw 09 done

--- homework09/js/main.js
+++ homework09/js/main.js
@@ -5,3 +5,34 @@ Advanced
 необхідно викликати функцію generateBlocksInterval();
 */
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateBlocks() {
+  const container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+
+  for (let i = 0; i < 25; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.backgroundColor = getRandomColor();
+    container.appendChild(block);
+  }
+}
+
+function generateBlocksInterval() {
+  generateBlocks();
+  setInterval(() => {
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+      block.style.backgroundColor = getRandomColor();
+    });
+  }, 1000);
+}
+
+generateBlocksInterval();
