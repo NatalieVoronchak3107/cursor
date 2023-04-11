2d94e2278cb6a0e211307f8fc446a5128c5a3b05
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-11 19:44:49 +0300

    hw 10 done

--- homework10/main.js
+++ homework10/main.js
@@ -1,5 +1,38 @@
 /* Необхідно написати програму, яка буде зчитувати з клавіатури натискання клавіш та відтворювати унікальний звук для кожної клавіши 
 (клавіши можуть бути будь-які, звуки можуть бути будь-які, тільки вкажіть у верстці – які кнопки натискати). */
-window.addEventListener('keydown', function (el) {
-    console.log(el);
-}) 
+function stopAllAudio() {
+  const allAudio = document.querySelectorAll("audio");
+  allAudio.forEach((audio) => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
+
+function playSoundAndChangeBackground(keyCode) {
+  const audio = document.querySelector(`audio[id="${keyCode}"]`);
+  const img = document.querySelector(`img[id="img-${keyCode}"]`);
+  const keyElement = document.querySelector(`.key[id="${keyCode}"]`);
+
+  if (!audio) return;
+
+  stopAllAudio();
+
+  audio.currentTime = 0;
+  audio.play();
+
+  const visibleImage = document.querySelector("img.visible");
+  if (visibleImage) visibleImage.classList.remove("visible");
+  img.classList.add("visible");
+
+  keyElement.classList.add("playing");
+}
+
+// Видалити клас "playing" після анімації
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("click", () => playSoundAndChangeBackground(key.id));
+  key.addEventListener("transitionend", function (event) {
+    if (event.propertyName !== "transform") return;
+    this.classList.remove("playing");
+  });
+});
