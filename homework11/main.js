62f9f3c3c0e0bfe563341d0da167907d24ef0f7a
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-12 00:07:06 +0300

    hw 11 done

--- homework11/main.js
+++ homework11/main.js
@@ -4,3 +4,26 @@
 виклику методу Date.now() Наприклад отримали const sign = 16086; Конвертуємо отриману цифу в рядок String.fromCharCode(sign); та чекаємо 50 ms
 Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами.
 Час роботи проміса має складати length * 50ms. (Якщо викликати getRandomChinese(4), отримаємо результат "촛궻簽紙" за 200ms */
+
+async function getRandomChinese(length) {
+  let result = "";
+
+  const createChineseCharacter = () => {
+    const sign = Date.now().toString().slice(-5);
+    return String.fromCharCode(sign);
+  };
+
+  const hold = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  while (length > 0) {
+    result += createChineseCharacter();
+    await hold(50);
+    length--;
+  }
+
+  return result;
+}
+
+getRandomChinese(4).then((result) => console.log(result));
